4ce9e5b5a0b4ff7836c63791c3f0a1a408fae455
Andey Shaporev <ashaporev@list.ru>
2023-11-12 17:53:06 +0400

    опредение роута custom

--- app/main.py
+++ app/main.py
@@ -5,5 +5,12 @@ app = FastAPI()
 
 #пример роута
 @app.get("/")
-async def root():
-    return FileResponse('templates/index.html')
+# async def root():
+#     return FileResponse('templates/index.html')
+def read_root():
+    return {"message": "Hello World!!!"}
+
+# новыйй роут
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!!!"}
--- main.py
+++ main.py
@@ -4,5 +4,12 @@ from fastapi.responses import  FileResponse
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return FileResponse('index.html')
+# async def root():
+#     return FileResponse('index.html')
+def read_root():
+    return {"message": "Hello World!!!"}
+
+# новыйй роут
+@app.get("/custom")
+def read_custom_message():
+    return {"message": "This is a custom message!!!"}
