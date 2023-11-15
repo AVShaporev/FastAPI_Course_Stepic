94ed83a37ba554a3b4b9418d2180f32829dc5021
Andrey Shaporev <ashaporev@list.ru>
2023-11-15 21:13:52 +0400

    2.3.5

--- app/main.py
+++ app/main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI
-from fastapi.responses import  FileResponse
+# from fastapi import FastAPI
+# from fastapi.responses import  FileResponse
 
-app = FastAPI()
+# app = FastAPI()
 
-#пример роута
-@app.get("/")
-# async def root():
-#     return FileResponse('templates/index.html')
-def read_root():
-    return {"message": "Hello World!!!"}
+# #пример роута
+# @app.get("/")
+# # async def root():
+# #     return FileResponse('templates/index.html')
+# def read_root():
+#     return {"message": "Hello World!!!"}
 
-# новыйй роут
-@app.get("/custom")
-def read_custom_message():
-    return {"message": "This is a custom message!!!"}
+# # # новыйй роут
+# # @app.get("/custom")
+# # def read_custom_message():
+# #     return {"message": "This is a custom message!!!"}
+
+# # пример пользовательских данных (демонстрация)
+# fake_users = {
+#     1: {"username": "john_doe", "email": "john@example.com"},
+#     2: {"username": "jane_smith", "email": "jane@example.com"},
+# }
+
+# # конечная точка для получения информации о пользователе
+# @app.get("/users/{user_id}")
+# def read_user(user_id: int):
+#     if user_id in fake_users:
+#         return fake_users[user_id]
+#     return {f"error: user {user_id} not found"}
--- main.py
+++ main.py
@@ -20,6 +20,17 @@ def read_root():
 def read_custom_message():
     return {"message": "This is a custom message!!!"}
 
-@app.get("/user")
-def read_user():
-    return JSONResponse(content=user.dict())
+# @app.get("/user")
+# def read_user():
+#     return JSONResponse(content=user.dict())
+
+# пример пользовательских данных (демонстрация)
+fake_users = {
+    1: {"username": "john_doe", "email": "john@example.com"},
+    2: {"username": "jane_smith", "email": "jane@example.com"},
+}
+
+# конечная точка для получения информации о пользователе
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return fake_users.get(user_id, {f"error: user with id - {user_id} not found"})
